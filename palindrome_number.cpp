e539cbfc5a0c846aad3ea6386ca58471dd93d00b
m00849351-hub <m00849351@gmail.com>
2026-02-01 13:07:05 +0530

    palindrome_number.cpp

---
+++ palindrome_number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, num, digit, rev = 0;
+    cout << "Enter a positive number: ";
+    cin >> num;
+    n = num;
+    
+    while(num != 0) {
+        digit = num % 10;
+        rev = (rev * 10) + digit;
+        num = num / 10;
+    }
+    if(n == rev)
+    cout << n << " is a palindrome.";
+    else
+    cout << n << " is not a palindrome.";
+    return 0;
+}
