6df70399b084e8a6d39354e2606bee8f7c662cb6
m00849351-hub <m00849351@gmail.com>
2026-02-02 14:13:15 +0530

    array_min.cpp

---
+++ array_min.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of element: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    
+    int min = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > min) min = arr[i];
+    }
+    cout << "Smallest element: " << min << endl;
+    return 0;
+}
