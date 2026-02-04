f11f7b27677d78c1e0240bbcd3237260ea2747ae
m00849351-hub <m00849351@gmail.com>
2026-02-04 15:36:17 +0530

    encapsulation.cpp

---
+++ encapsulation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+    private:
+   int salary;
+   public:
+   void setSalary(int s) {
+       if(s > 0) salary = s;
+   }
+   int getSalary() {
+       return salary;
+   }
+};
+int main() {
+    Employee emp;
+    emp.setSalary(50000);
+    cout << "Salary: " << emp.getSalary() << endl;
+    return 0;
+}
